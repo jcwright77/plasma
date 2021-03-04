da177c5c814b90eb8a03e5e51923519a487b1d20
jcwright77 <jcwright@mit.edu>
2021-03-04 11:32:47 -0500

    Improved behavior of Z(psi,theta=0)
    Z(psi,theta=0) is now zero within roundoff. Should improve noise in metric coefficients.
    jacobian option to straight or equal arc length.

--- mapper.py
+++ mapper.py
@@ -1,39 +1,38 @@
-#mapper program. Expects eq variable to be defined with eqdsk function.
-
+#put it all together
 import scipy.integrate as integrate
 import scipy.fftpack as sft
-from scipy import integrate
-import scipy.interpolate
+import scipy.interpolate as interpolate
 
 mu0=4.*np.pi*1.e-7
 R=eq.get('r')
 Z=eq.get('z')
-B,gpsi,fRZ,Rv,Zv=eqdsk.getModB(eq)
+B,grad_psi,fRZ,Rv,Zv=eqdsk.getModB(eq)
 psi=eq.get('psizr').T
 curtor=[]
 area=[]
 
+mapzmaxis=0.0
+
 ifrhopol=True #use root psipol mesh instead of psipol (eg for torlh)
+jac='straight' #'eqarc'
 
 def find_cut(x,y, rmaxis, zmaxis):
-  #from S. Shiraiwai to find crossing going counter clockwise
+  #adapted from S. Shiraiwai to find crossing going counter clockwise
   for k in range(len(y)-1):
        km = k-1
        if y[km] < zmaxis and y[k] > zmaxis:
          return k
-    #('findcut',x,y)
   return -1
-
-#ensure periodicity with fft and reduce noise, esp near axis
 
 # get flux surface on fine rectangular mesh
 newR=np.arange(min(R),max(R),0.01)
 newZ=np.arange(min(Z),max(Z),0.01)
+newZ=newZ-eq['zmaxis']  #axis needs to be at z=0 for mapping
 
 RR,ZZ=np.mgrid [min(R):max(R):129j, min(Z):max(Z):129j ] #129x129 2D coordinate meshes
 RR_finer,ZZ_finer=np.mgrid [min(R):max(R):200j, min(Z):max(Z):200j ] #200x200 2D coordinate meshes
 
-spline_psi = scipy.interpolate.RectBivariateSpline(R,Z,psi.T,bbox=[np.min(R),np.max(R),np.min(Z),np.max(Z)],kx=5,ky=5)
+spline_psi = interpolate.RectBivariateSpline(R,Z,psi.T,bbox=[np.min(R),np.max(R),np.min(Z),np.max(Z)],kx=5,ky=5)
 psi_int=spline_psi.ev(RR_finer,ZZ_finer)
 psi_int_r=spline_psi.ev(RR_finer,ZZ_finer,dx=1)
 psi_int_z=spline_psi.ev(RR_finer,ZZ_finer,dy=1)
@@ -81,7 +80,7 @@ except FileNotFoundError:
   psimesh = np.interp(fitxvals, fitx, fity) #this just stretches fity to size of npsi
 
   rmaxis = eq['rmaxis']
-  zmaxis = eq['zmaxis']
+  zmaxis = mapzmaxis #eq['zmaxis']
   eq['psipolmap'] = psimesh
 
   c_pprime = np.interp( psimesh, eq['fluxGrid'], eq['pprime'] )
@@ -95,6 +94,7 @@ except FileNotFoundError:
   ax.set_aspect('equal')
   fig.set_figheight(6)
   psi_cs=plt.contour(RR_finer,ZZ_finer,psi_int,levels=psimesh)
+  #psi_cs=plt.contour(Rv,Zv,psi,levels=psimesh)
 
 #Get Psi contours, careful to exclude field coils
   psixy=[]
@@ -110,50 +110,63 @@ except FileNotFoundError:
   uni_theta=np.linspace(0,2.0*np.pi,ntheta,endpoint=False)
 
 #Set up X(psi,theta) Y(psi,theta) and initialize with magnetic axis point
-#actually, do NOT add axis point
   eq_x=[]
-#eq_x.append(np.array([rmaxis]*ntheta))
   eq_y=[]
-#eq_y.append(np.array([zmaxis]*ntheta))
 
+  #is there any reason for two meshes here?
   points = np.array( (RR_finer.flatten(), ZZ_finer.flatten()) ).T
-  psivalues = grad_psi.flatten()
+  #points = np.array( (Rv.flatten(), Zv.flatten()) ).T
+  gpsivalues = grad_psi.flatten()
   Bpoints = np.array( (RR.flatten(), ZZ.flatten()) ).T
   Bvalues   = B.flatten()
 
   from scipy.interpolate import griddata
-  print('points',points.shape,psivalues.shape)
+  #print('points',points.shape,gpsivalues.shape)
   c_idx=-1
+  minmodes = 8
+  maxmodes = 20
   for cx,cy in psixy:
       c_idx+=1
       print('cx',c_idx, len(cx),len(psixy)) #just to see progress
       #for each surface, low pass filter to central 8+ DC Fourier modes
       #remove last element for fft since it is equal to the first element
       #size of (cx,cy) is  variable
-      minmodes = 8
-      maxmodes = 20
+
+      #shift to midplane as first element
+      idx=find_cut(cx,cy,rmaxis,zmaxis)
+      if (idx>=0):
+          #now project to centers
+          #this assumes that values stradle midplane which seems to be the 
+          #case for python contour, but should be made more robust.
+          cx=0.5*(np.roll(cx,-idx)+np.roll(cx,-idx+1))
+          cy=0.5*(np.roll(cy,-idx)+np.roll(cy,-idx+1))
+
+      #print('0',cy[0],cy[-1],cx[0],cx[-1],np.average(cy))
+
+      #filter out high freq noise, esp needed near axis
       nmodes=max( minmodes,int(len(cx)/np.float(maxmodes) ))
-      fftx=sft.fft(cx[0:-1])
+      fftx=sft.fft(cx)
       fftx[int(nmodes/2)+1:-int(nmodes/2)]=0
       filtered_cx=sft.ifft(fftx).real
         
-      ffty=sft.fft(cy[0:-1]) #fft makes filtered_cx/y complex
+      ffty=sft.fft(cy)
       ffty[int(nmodes/2)+1:-int(nmodes/2)]=0
+      
+      #restore value for idx=0 for Y. 
+      ffttotal=np.sum(ffty) #want to restore to total before filter.
+      ffty[int(nmodes/2)+1]=-ffttotal/2.
+      ffty[-int(nmodes/2)-1]=-ffttotal/2.
+
       filtered_cy=sft.ifft(ffty).real
 
-    #shift contours and insert outer midplane value so we have theta=0 value
-    #an important step to remove noise
-
-      idx=find_cut(filtered_cx,filtered_cy,rmaxis,zmaxis)
-      if (idx>=0):
-          filtered_cx=np.roll(filtered_cx,-idx)
-          filtered_cy=np.roll(filtered_cy,-idx)
-          #print('rolled',filtered_cy[0],filtered_cy[-1])
     #interpolate |grad psi| and B onto this surface
     #this significantly slows down this routine. I think this can be moved outside of the 
     #loop if cx,cy are saved.
-      c_gradpsi = griddata( points, psivalues, (filtered_cx,filtered_cy), method='cubic' )
-      c_B       = griddata( Bpoints,   Bvalues, (filtered_cx,filtered_cy), method='cubic' )
+
+      print('1',filtered_cy[1],filtered_cy[0],filtered_cy[-1],filtered_cx[0])
+
+      c_gradpsi = griddata( points, gpsivalues, (filtered_cx,filtered_cy), method='cubic' )
+      c_B       = griddata( Bpoints,    Bvalues, (filtered_cx,filtered_cy), method='cubic' )
 
     #derivative from fft needs factor of 2pi
       df_dx=sft.diff(filtered_cx)*np.pi*2.0/len(filtered_cx)
@@ -161,7 +174,6 @@ except FileNotFoundError:
       dl=np.sqrt(df_dx**2+df_dy**2) #These two steps could be done with FFT too.
 
       #flux surface integrals here
-      print(c_idx, np.sum(dl) , integrate.simps(dl), np.average(1/c_gradpsi)*(psimesh[c_idx]-psimesh[c_idx+1]))
       c_curtor = -integrate.simps(
           dl*filtered_cx/c_gradpsi * ( c_pprime[c_idx] + c_ffprime[c_idx]/filtered_cx**2/mu0 ) 
       )
@@ -169,21 +181,21 @@ except FileNotFoundError:
           dl/c_gradpsi
       )
 
-
       #if straight field line, multiply dl by 1/R*|gradpsi|
-      dtheta=dl/np.abs(c_gradpsi*filtered_cx)
+      if jac=='straight':
+        dtheta=dl/np.abs(c_gradpsi*filtered_cx)
+      else: #jac='eqarc'
+        dtheta=dl
       L=integrate.cumtrapz(dtheta,initial=0)/len(dtheta) 
-      #L=np.insert(L,0,0)
-
+      
       #now put each on same theta mesh, Jacobian selection
       this_theta=L/np.max(L)*2.0*np.pi
-      t_map=scipy.interpolate.interp1d(this_theta,filtered_cx,kind='cubic')
+      t_map=interpolate.interp1d(this_theta,filtered_cx,kind='cubic')
       eq_x.append(t_map(uni_theta))
-      t_map=scipy.interpolate.interp1d(this_theta,filtered_cy,kind='cubic')
+      t_map=interpolate.interp1d(this_theta,filtered_cy,kind='cubic')
       eq_y.append(t_map(uni_theta))
       curtor.append (c_curtor)
       area.append( c_area )
-
 
   #add origin term
   area.insert(0,0.)          #area of origin is 0. 
@@ -197,10 +209,13 @@ except FileNotFoundError:
   #add origin
   NXmap=np.zeros([npsi,ntheta])
   NZmap=np.zeros([npsi,ntheta])
-  NXmap[0,:]=eq['rmaxis']
-  NZmap[0,:]=eq['zmaxis']
+  NXmap[0,:]=eq['rmaxis'] #origin for all theta
+  NZmap[0,:]=mapzmaxis #eq['zmaxis']
   NXmap[1:,:]=Xmap
   NZmap[1:,:]=Zmap
+
+  #print("Error of axis is",100.*np.average(np.abs(Zmap[0,:]-mapzmaxis))/mapzmaxis )
+
   del(Xmap)
   del(Zmap)
   Xmap=NXmap
